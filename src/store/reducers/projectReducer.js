db5d2469d8665505d3505e009295956583e57fbd
Amnok <ankitsingh80741@gmail.com>
2021-01-25 23:53:34 +0530

    dumy data added

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initialState = {};
+const initialState = {
+    projects: [
+        {id: 1, title: 'xyz', content: 'sakldas dsa d sad salkdjsklajdsa dslajkdnlskajdklsad sa dasklcxzc xz cxzcxzc xsda d sad sa d sad'},
+        {id: 2, title: 'xyz1', content: 'sakldas dsa d sad salkdjsklajdsa dslajkdnlskajdklsad sa dasklcxzc xz cxzcxzc xsda d sad sa d sad'},
+        {id: 3, title: 'xyz2', content: 'sakldas dsa d sad salkdjsklajdsa dslajkdnlskajdklsad sa dasklcxzc xz cxzcxzc xsda d sad sa d sad'}
+    ]
+};
 const projectReducer = (state = initialState, action) => {
 return state;
 }
